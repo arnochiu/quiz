c188a762aea9dbe42fca7f6fbac5e484f0bdf3ca
arnochiu <arnoishandsome@gmail.com>
2015-10-16 14:03:15 +0800

    implement iterative way to find target char
    compare char in the input array with another input char,
    until target char being found or the end of array(ascii(127))

--- q2/iterative.c
+++ q2/iterative.c
@@ -1,6 +1,19 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+
+char smallest_character(char str[], char c){
+	int size;
+	for(size=0; str[size]!=127; size++)
+		if(str[size] > c)
+			return str[size];
+	return str[0];
+}
 
 int main()
 {
-    return 0;
+	char in[] = {'c', 'f', 'j', 'p', 'v'};
+	
+	printf("%c\n", smallest_character(in, 'c'));
+
+	return 0;
 }
