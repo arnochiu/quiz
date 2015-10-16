887869f6f57e9e116d0f78bcc432e77053854465
arnochiu <arnoishandsome@gmail.com>
2015-10-16 14:44:24 +0800

    implement recursive way to find target char

--- q2/recursive.c
+++ q2/recursive.c
@@ -1,6 +1,21 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+
+char smallest_character(char str[], char c, int index){
+	if(str[index]!=127){
+		if(str[index] > c)
+			return str[index];
+		else
+			return smallest_character(str, c, (index+1));
+	}
+	return str[0];
+}
 
 int main()
 {
-    return 0;
+	char in[] = {'c', 'f', 'j', 'p', 'v'};
+	
+	printf("%c\n", smallest_character(in, 'z', 0));
+
+	return 0;
 }
