07af3701113a54d29e322f250e4e5180873ad7db
arnochiu <arnoishandsome@gmail.com>
2015-10-17 22:05:27 +0800

    implement maxSubArray() in iterative way

--- q4/iterative.c
+++ q4/iterative.c
@@ -1,6 +1,28 @@
 /* FIXME: Implement! */
+#include <stdio.h>
+int maxSubArray(int A[], int n){
+	if(n!=0){
+		int max_so_far = A[0];
+		int curr_max = A[0];
+		for(int i=1; i<n; i++){
+			curr_max = (A[i]>curr_max+A[i])? A[i] : curr_max+A[i];
+			max_so_far = (max_so_far>curr_max)? max_so_far : curr_max;
+		}
+		return max_so_far;
+	}
+	else
+		return 0;
+}
 
 int main()
 {
-    return 0;
+	int in[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+	int length = sizeof(in) / sizeof(int);
+	int out;
+
+	out = maxSubArray(in, length);
+
+	printf("largest sum: %d\n", out);
+
+	return 0;
 }
